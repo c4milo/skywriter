8ffff65f18c068b46cccea704110d8879d9e77b5
Camilo Aguilar <camilo.aguilar@gmail.com>
2010-09-30 13:15:07 -0400

    [dryice] jlint, cleaning unused files

--- dryice.js
+++ dryice.js
@@ -20,7 +20,7 @@
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
- *   Bespin Team (skywriter@mozilla.com)
+ *   Skywriter Team (skywriter@mozilla.com)
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
--- dryice/lib/plugin.js
+++
@@ -1,6 +0,0 @@
-var exports = module.exports;
-
-var Plugin = exports.Plugin = function() {
-
-}
-
